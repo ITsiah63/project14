ecd323aa210c141df1b68f5bbd16c71913391042
Isiah Reyes <i0576631@amarillocollege.com>
2025-04-14 15:32:26 -0500

    first commit

---
+++ project01-04.js
@@ -0,0 +1,21 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: isiah reyes
+    Date:   04-14-25
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+
+var homeStreet = "1 Main St."
+var homeCity = "Sicilia"
+var homeCode = "02103"
+var workStreet = "15 Oak Ln"
+var workCity = "Central City"
+var workState = "MA"
+var workCode = "02104"
+
+
